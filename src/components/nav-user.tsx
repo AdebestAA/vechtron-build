d0b887c6d52f00af92cb7129f1e0b92be0aab222
Leke Abiodun <lekepeterabiodun@gmail.com>
2025-08-12 22:50:13 +0100

    Update nav-user.tsx

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -4,11 +4,9 @@ import {
   BadgeCheck,
   Bell,
   ChevronDown,
-  ChevronsDown,
-  ChevronsUpDown,
   CreditCard,
   LogOut,
-  Sparkles,
+  Sparkles
 } from "lucide-react"
 
 import {
@@ -72,7 +70,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage  src={user.avatar} alt={user.name} />
+                  <AvatarImage src={user.avatar} alt={user.name} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
